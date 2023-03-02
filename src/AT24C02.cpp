8a4165ed64304d3334c5542d849ca996aa060058
TomaszOp <98652048+TomaszOp@users.noreply.github.com>
2023-03-02 21:41:04 +0100

    Add files via upload

--- src/AT24C02.cpp
+++ src/AT24C02.cpp
@@ -2,9 +2,17 @@
 
 #include "AT24C02.h"
 
-AT24C02::AT24C02(byte _address)
+AT24C02::AT24C02(byte _addressI2C)
 {
-  this->addressI2C = _address;
+  this->addressI2C = _addressI2C;
+
+  this->SdaPin = DEFAULT_EEPROM_SdaPin;   
+  this->SclPin = DEFAULT_EEPROM_SclPin;
+}
+
+AT24C02::AT24C02()
+{
+  this->addressI2C = DEFAULT_EEPROM_i2C_ADDRESS;
 
   this->SdaPin = DEFAULT_EEPROM_SdaPin;   
   this->SclPin = DEFAULT_EEPROM_SclPin;
@@ -16,16 +24,31 @@ void AT24C02::AttachI2CPins(int _sdaPin, int _sclPin)
   this->SclPin = _sclPin;      
 }
  
+void AT24C02::SetAddressI2C(byte _addressI2C)
+{
+  this->addressI2C = _addressI2C;
+}
+
 void AT24C02::Init()
 {
   Wire.begin(this->SdaPin, this->SclPin);
 }
 
-void AT24C02::Write(unsigned int _offset, byte _data)
+void AT24C02::Init(int _sdaPin, int _sclPin, byte _address)
+{
+  this->SdaPin = _sdaPin;   
+  this->SclPin = _sclPin; 
+
+  this->addressI2C = _address;
+
+  Wire.begin(this->SdaPin, this->SclPin);
+}
+
+void AT24C02::Write(byte _address, byte _data)
 {
   Wire.beginTransmission(this->addressI2C);
 
-  Wire.write((int)_offset);
+  Wire.write(_address);
 
   Wire.write(_data);
   Wire.endTransmission();
@@ -33,27 +56,83 @@ void AT24C02::Write(unsigned int _offset, byte _data)
   delay(EEPROM_DELAY);
 }
 
-void AT24C02::WriteBuffer(unsigned int _offset, byte * _data, unsigned int _length)
+void AT24C02::writePage(byte _address, byte _length, byte* _ptr_data)
 {
-  Wire.beginTransmission(this->addressI2C);
-  Wire.write((int)_offset);
+    // Write complete buffers.
+    byte bufferCount = _length / EEPROM__WR_BUFFER_SIZE;
+    for (byte i = 0; i < bufferCount; i++)
+    {
+        byte offset = i * EEPROM__WR_BUFFER_SIZE;
+        writeBuffer(_address + offset, EEPROM__WR_BUFFER_SIZE, _ptr_data + offset);
+    }
 
-  for(byte i = 0; i < _length; i++)
-  {
-    Wire.write(_data[i]);
-  }
-  
-  Wire.endTransmission();
-  delay(EEPROM_DELAY);
+    // Write remaining bytes.
+    byte remainingBytes = _length % EEPROM__WR_BUFFER_SIZE;
+    byte offset = _length - remainingBytes;
+    writeBuffer(_address + offset, remainingBytes, _ptr_data + offset);
 }
 
-byte AT24C02::Read(unsigned int _offset)
+void AT24C02::WriteBytes(byte _address, byte _length, byte* _ptr_data)
+{
+
+  // Write first page if not aligned.
+  byte notAlignedLength = 0;
+  byte pageOffset = _address % EEPROM__PAGE_SIZE;
+  if (pageOffset > 0)
+  {
+      notAlignedLength = EEPROM__PAGE_SIZE - pageOffset;
+      if (_length < notAlignedLength)
+      {
+          notAlignedLength = _length;
+      }
+      writePage(_address, notAlignedLength, _ptr_data);
+      _length -= notAlignedLength;
+  }
+
+  if (_length > 0)
+  {
+      _address += notAlignedLength;
+      _ptr_data += notAlignedLength;
+
+      // Write complete and aligned pages.
+      byte pageCount = _length / EEPROM__PAGE_SIZE;
+      for (byte i = 0; i < pageCount; i++)
+      {
+          writePage(_address, EEPROM__PAGE_SIZE, _ptr_data);
+          _address += EEPROM__PAGE_SIZE;
+          _ptr_data += EEPROM__PAGE_SIZE;
+          _length -= EEPROM__PAGE_SIZE;
+      }
+
+      if (_length > 0)
+      {
+          // Write remaining uncomplete page.
+          writePage(_address, _length, _ptr_data);
+      }
+  }
+}
+
+void AT24C02::writeBuffer(byte _address, byte _length, byte* _ptr_data)
+{
+    Wire.beginTransmission(this->addressI2C);
+    Wire.write(_address);
+    for (byte i = 0; i < _length; i++)
+    {
+        Wire.write(_ptr_data[i]);
+    }
+    Wire.endTransmission();
+    
+    delay(EEPROM_DELAY);
+}
+
+
+byte AT24C02::Read(byte _address)
 {
   Wire.beginTransmission(this->addressI2C);
-  Wire.write((int)_offset);
+  Wire.write(_address);
   Wire.endTransmission();
 
-  Wire.requestFrom(this->addressI2C, 1);
+  Wire.requestFrom((int)this->addressI2C, (int)1);
   
   byte data = 0;
   if (Wire.available())
@@ -65,22 +144,36 @@ byte AT24C02::Read(unsigned int _offset)
   return data;
 }
 
-void AT24C02::ReadBuffer(unsigned int _offset, unsigned int _length, byte * _buffer)
+void AT24C02::ReadBytes(byte _address, byte _length, byte* _ptr_data)
 {
-  Wire.beginTransmission(this->addressI2C);
-  Wire.write((int)_offset);
-  Wire.endTransmission();
+    byte bufferCount = _length / EEPROM__RD_BUFFER_SIZE;
+    for (byte i = 0; i < bufferCount; i++)
+    {
+        byte offset = i * EEPROM__RD_BUFFER_SIZE;
+        readBuffer(_address + offset, EEPROM__RD_BUFFER_SIZE, _ptr_data + offset);
+    }
 
-  Wire.write((int)_offset);
-  Wire.requestFrom(this->addressI2C, _length);
-  
-  for (byte i = 0; i < _length; i++)
-  {
-      if (Wire.available())
-      {
-          _buffer[i] = Wire.read();
-      }
-  }
+    byte remainingBytes = _length % EEPROM__RD_BUFFER_SIZE;
+    byte offset = _length - remainingBytes;
 
-  delay(EEPROM_DELAY);
+    if(remainingBytes > 0)
+    {
+      readBuffer(_address + offset, remainingBytes, _ptr_data + offset);
+    }
+}
+
+void AT24C02::readBuffer(byte _address, byte _length, byte* _ptr_data)
+{
+    Wire.beginTransmission(this->addressI2C);
+    Wire.write(_address);
+    Wire.endTransmission();
+
+    Wire.requestFrom(this->addressI2C, _length);
+    for (byte i = 0; i < _length; i++)
+    {
+        if (Wire.available())
+        {
+            _ptr_data[i] = Wire.read();
+        }
+    }
 }
--- src/AT24C02.h
+++ src/AT24C02.h
@@ -9,22 +9,38 @@
 #define DEFAULT_EEPROM_SdaPin 21
 #define DEFAULT_EEPROM_SclPin 22
 
+#define DEFAULT_EEPROM_i2C_ADDRESS 0x50
+
+#define BUFFER_LENGTH 32
+#define EEPROM__PAGE_SIZE 8
+#define EEPROM__RD_BUFFER_SIZE    BUFFER_LENGTH
+#define EEPROM__WR_BUFFER_SIZE    (BUFFER_LENGTH - 1)
+
 class AT24C02
 {
   public:
-    AT24C02(byte _address);
+    AT24C02(byte _addressI2C);
+    AT24C02();
 
     int SdaPin;
     int SclPin;
     void AttachI2CPins(int _sdaPin, int _sclPin);
+    void SetAddressI2C(byte _addressI2C);
 
     void Init();
-    void Write(unsigned int _offset, byte _data); //write single byte to EEPROM
-    void WriteBuffer(unsigned int _offset, byte* _buffer, unsigned int _length); //write multiple bytes to EEPROM
-    byte Read(unsigned int _offset); //read single byte from EEPROM
-    void ReadBuffer(unsigned int _offset, unsigned int _length, byte * _buffer); //read multiple bytes to EEPROM
+    void Init(int _sdaPin, int _sclPin, byte _addressI2C);
+
+    void Write(byte _address, byte _data); //write single byte to EEPROM
+    byte Read(byte _address); //read single byte from EEPROM
+
+    void WriteBytes(byte _address, byte _length, byte* _ptr_data);
+    void ReadBytes(byte _address, byte _length, byte* _ptr_data);
+
   private:
     byte addressI2C;
+    void writePage(byte _address, byte _length, byte* _ptr_data);
+    void writeBuffer(byte _address, byte _length, byte* _ptr_data);
+    void readBuffer(byte _address, byte _length, byte* _ptr_data);
 };
 
 #endif
